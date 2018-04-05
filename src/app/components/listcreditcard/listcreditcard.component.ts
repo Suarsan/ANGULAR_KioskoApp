d31c6fd14f855b42a3e0877609d5f1ea5aab3c8f
Suarsan <javiersuarsan@gmail.com>
2018-04-05 13:11:21 +0200

    Terminado wallet.component

--- src/app/components/listcreditcard/listcreditcard.component.ts
+++ src/app/components/listcreditcard/listcreditcard.component.ts
@@ -3,6 +3,7 @@ import { UserService } from '../../services/user-service/user.service';
 import { WalletModel } from '../../models/wallet.model';
 import { WalletService } from '../../services/wallet-service/wallet.service';
 import { UserModel } from '../../models/user.model';
+import { CreditcardModel } from '../../models/creditcard.model';
 
 @Component({
   selector: 'app-listcreditcard',
@@ -18,17 +19,18 @@ export class ListCreditCardComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getWallet();
+  }
+
+  getWallet() {
     this.walletService.getWalletByUserId(this.currentUser.Id).subscribe(
       receivedWallet => {
         if ( receivedWallet.length > 0) {
           this.wallet = new WalletModel(0);
           Object.assign(this.wallet,  receivedWallet[0]);
           console.log(this.wallet);
-
         }
       }
     );
-
   }
-
 }
