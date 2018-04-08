b2c813d592525deb46f306faf12306803d671310
AzaharaBM <azbalmar@gmail.com>
2018-04-08 13:07:27 +0200

    Legal pages already done.

--- src/app/therms/therms.component.ts
+++ src/app/therms/therms.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-therms',
   templateUrl: './therms.component.html',
-  styleUrls: ['./therms.component.sass']
+  styleUrls: ['./therms.component.less']
 })
 export class ThermsComponent implements OnInit {
 
