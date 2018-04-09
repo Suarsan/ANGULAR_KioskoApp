2d30a593525a02aa9d79247ac627497937ce9681
Azahara <35915786+azaharabm@users.noreply.github.com>
2018-04-09 15:18:09 +0200

    as

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth-service/auth.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.sass']
+  styleUrls: ['./contact.component.less']
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  public nombre: string;
+  public apellidos: string;
+  public email: string;
+  public mensaje: string;
+  public agree = false;
+  public message = '';
+
+
+  constructor(private sendMeauthService: AuthService ) { }
 
   ngOnInit() {
+  }
+
+  private Accept() {
+    console.dir(this.agree);
+
   }
 
 }
