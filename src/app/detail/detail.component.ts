39b29ba4e34f5680648428e5b87feb55698f71ee
Suarsan <javiersuarsan@gmail.com>
2018-04-09 16:13:35 +0200

    Primer commit tras clone

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,35 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MagazineModel } from '../models/magazine.model';
-import { PublisherModel } from '../models/publisher.model';
-import { MagazineDaoService } from '../../dao/magazine-dao/magazine-dao.service';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.sass']
 })
 export class DetailComponent implements OnInit {
 
-  private currentMagazine: MagazineModel;
-  private id: string | number;
+  id: MagazineModel;
 
-  constructor( private magazineDAO: MagazineDaoService ) { }
+  constructor() { }
 
   ngOnInit() {
-    this.currentMagazine = new MagazineModel("1", "NombreRevista", new PublisherModel("1", "EditorialRevista"), "Descripcion revista", "categoria", ["tag1", "tag2"], ["thumbnail1", "thumbnail2"], ["downloadUrl1", "downloadUrl2"], ["contentUrl1", "contentUrl2"], 4);
-    this.currentMagazine.Title = '1';
-    this.getMagazine(1);
-  }
-
-  private getMagazine(id) {
-    this.magazineDAO.get(id).subscribe(
-      data => {
-        this.currentMagazine = data;
-      },
-      err => {
-        console.log('ERROR');
-      }
-    );
   }
 
 }
